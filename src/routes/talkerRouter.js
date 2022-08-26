70bf06f30cf46988f5802fa999288e4c37266d56
VictorFranciscoFerreira <victorfranciscoferreira@gmail.com>
2022-08-26 17:41:54 -0300

    req2:feito

--- src/routes/talkerRouter.js
+++ src/routes/talkerRouter.js
@@ -12,4 +12,18 @@ router.get('/', async (_req, res) => {
   }
 });
 
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const talkers = await readFile(); 
+    const users = talkers.find((user) => user.id === Number(id));
+    if (!users) {
+      return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+    }
+    return res.status(200).json(users);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
 module.exports = router;
